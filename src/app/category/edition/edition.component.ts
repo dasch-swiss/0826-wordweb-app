9d4ad0e6ae246fb6f627e3acd6b0df037237a88c
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-05-16 17:27:05 +0200

    edition view

--- src/app/category/edition/edition.component.ts
+++ src/app/category/edition/edition.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {MatDialog, MatDialogConfig, MatSort, MatTableDataSource} from "@angular/material";
+import {ApiService, Author, Edition} from "../../api.service";
+import {AuthorRefComponent} from "../../dialog/author-ref/author-ref.component";
 
 @Component({
   selector: "app-edition",
@@ -7,9 +10,53 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EditionComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ["publicationInfo", "book", "language", "references", "action"];
+  dataSource: MatTableDataSource<Edition>;
+  value: string;
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private apiService: ApiService,
+              private authorDialog: MatDialog) {
+    this.dataSource = new MatTableDataSource(this.apiService.getEditions());
+  }
 
   ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  clear() {
+    this.dataSource.filter = this.value = "";
+  }
+
+  rowCount() {
+    return this.dataSource.filteredData.length;
+  }
+
+  copyArray(authors: Author[]) {
+    return authors;
+  }
+
+  editBook(books: any[]) {
+  //   const dialogConfig = new MatDialogConfig();
+  //   dialogConfig.disableClose = true;
+  //   dialogConfig.autoFocus = true;
+  //   dialogConfig.data = {
+  //     values: this.copyArray(editions),
+  //     editMod: true
+  //   };
+  //   const dialogRef = this.authorDialog.open(AuthorRefComponent, dialogConfig);
+  //   dialogRef.afterClosed().subscribe((data) => {
+  //     console.log(data);
+  //   });
+  }
+
+  editLanguage(language: any) {
+
   }
 
 }
