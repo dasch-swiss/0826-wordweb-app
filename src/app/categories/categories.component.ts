a57746bb99152117c09655a2b2ee13698850adc2
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-10-04 16:12:30 +0200

    including knora-core

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,93 +1,126 @@
-import { Component, OnInit } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
-// import {ApiServiceError, Project, ProjectsService} from "@knora/core";
+import {
+  ApiServiceError,
+  ApiServiceResult,
+  ListNode,
+  ListsService,
+  OntologyService,
+  Project,
+  ProjectsService,
+  ResourceService
+} from "@knora/core";
 
 @Component({
-  selector: "app-categories",
-  templateUrl: "./categories.component.html",
-  styleUrls: ["./categories.component.scss"]
+    selector: "app-categories",
+    templateUrl: "./categories.component.html",
+    styleUrls: ["./categories.component.scss"]
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor(
-    private router: Router,
-    // private projectService: ProjectsService
-  ) {
-  }
+    constructor(
+        private router: Router,
+        private projectService: ProjectsService,
+        private listService: ListsService,
+        private ontologyService: OntologyService,
+        private resourceService: ResourceService
+    ) {
+    }
 
-  ngOnInit() {
-    // this.projectService.getProjectByShortcode("0826")
-    //   .subscribe((data: Project) => {
-    //       console.log(data);
-    //     },
-    //     (error: ApiServiceError) => {
-    //       console.error(error);
-    //     }
-    //   );
-  }
+    ngOnInit() {
+        // this.projectService.getProjectByShortcode("0826")
+        //     .subscribe((data: Project) => {
+        //             console.log(data);
+        //         },
+        //         (error: ApiServiceError) => {
+        //             console.error(error);
+        //         }
+        //     );
+        //
+        // this.listService.getLists("http://rdfh.ch/projects/0826")
+        //     .subscribe((data: ListNode[]) => {
+        //         console.log(data);
+        //     }, (error: ApiServiceError) => {
+        //         console.error(error);
+        //     });
+        //
+        // this.ontologyService.getProjectOntologies("http://rdfh.ch/projects/0826")
+        //     .subscribe((result: ApiServiceResult) => {
+        //       console.log(result);
+        //     }, (error: ApiServiceError) => {
+        //       console.error(error);
+        //     });
+        //
+        // this.resourceService.getResource("http://rdfh.ch/0826/MfU3t95jQiqQ72dmI_bzdQ")
+        //     .subscribe((data: any) => {
+        //       console.log(data);
+        //     }, (error: ApiServiceError) => {
+        //       console.error(error);
+        //     });
+    }
 
-  showAuthors() {
-    this.router.navigate(["author"]);
-  }
+    showAuthors() {
+        this.router.navigate(["author"]);
+    }
 
-  showBooks() {
-    this.router.navigate(["book"]);
-  }
+    showBooks() {
+        this.router.navigate(["book"]);
+    }
 
-  showContributors() {
-    this.router.navigate(["contributor"]);
-  }
+    showContributors() {
+        this.router.navigate(["contributor"]);
+    }
 
-  showFunctionVoices() {
-    this.router.navigate(["function"]);
-  }
+    showFunctionVoices() {
+        this.router.navigate(["function"]);
+    }
 
-  showGenders() {
-    this.router.navigate(["gender"]);
-  }
+    showGenders() {
+        this.router.navigate(["gender"]);
+    }
 
-  showGenres() {
-    this.router.navigate(["genre"]);
-  }
+    showGenres() {
+        this.router.navigate(["genre"]);
+    }
 
-  showImages() {
-    this.router.navigate(["image"]);
-  }
+    showImages() {
+        this.router.navigate(["image"]);
+    }
 
-  showLanguages() {
-    this.router.navigate(["language"]);
-  }
+    showLanguages() {
+        this.router.navigate(["language"]);
+    }
 
-  showLexias() {
-    this.router.navigate(["lexia"]);
-  }
+    showLexias() {
+        this.router.navigate(["lexia"]);
+    }
 
-  showMarkings() {
-    this.router.navigate(["marking"]);
-  }
+    showMarkings() {
+        this.router.navigate(["marking"]);
+    }
 
-  showOrganisations() {
-    this.router.navigate(["organisation"]);
-  }
+    showOrganisations() {
+        this.router.navigate(["organisation"]);
+    }
 
-  showPassages() {
-    this.router.navigate(["passage"]);
-  }
+    showPassages() {
+        this.router.navigate(["passage"]);
+    }
 
-  showResearchFields() {
-    this.router.navigate(["research"]);
-  }
+    showResearchFields() {
+        this.router.navigate(["research"]);
+    }
 
-  showStatus() {
-    this.router.navigate(["status"]);
-  }
+    showStatus() {
+        this.router.navigate(["status"]);
+    }
 
-  showSubjects() {
-    this.router.navigate(["subject"]);
-  }
+    showSubjects() {
+        this.router.navigate(["subject"]);
+    }
 
-  showVenues() {
-    this.router.navigate(["venue"]);
-  }
+    showVenues() {
+        this.router.navigate(["venue"]);
+    }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,33 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+function bootstrapFailed(result) {
+  console.error("bootstrap-fail", result);
+}
+
+fetch(`config/config.${environment.name}.json`)
+    .then(response => response.json())
+    .then(config => {
+      if (!config || !config["appName"]) {
+        bootstrapFailed(config);
+        return;
+      }
+
+      // Store the response somewhere that your ConfigService can read it.
+      window["tempConfigStorage"] = config;
+
+      // console.log("config", config);
+
+
+      platformBrowserDynamic()
+          .bootstrapModule(AppModule)
+          .catch(err => bootstrapFailed(err));
+    })
+    .catch(bootstrapFailed);
