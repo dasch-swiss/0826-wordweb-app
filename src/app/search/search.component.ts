54bd05419e82f3f9e3b25b9a150881e8f72e5e95
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2020-03-16 17:59:24 +0100

    Further gravesearch templates

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -16,9 +16,15 @@ export class SearchComponent implements OnInit {
             .subscribe(bla => {
                 console.log(bla);
                 this.knoraService.getPassageWithText("Queen Margaret")
-                    .subscribe(blabla => console.log(blabla));
-                this.knoraService.getPassageWithBookTitle("Hamlet")
-                    .subscribe(a => console.log(a));
+                    .subscribe(res1 => console.log(res1));
+                // this.knoraService.getPassageWithBookTitle("Hamlet")
+                //     .subscribe(res2 => console.log(res2));
+                // this.knoraService.getAllBooks()
+                //     .subscribe(res3 => console.log(res3));
+                // this.knoraService.getSecBooks()
+                //     .subscribe(sec => console.log(sec));
+                this.knoraService.getPassageAuthor("http://rdfh.ch/0826/il495XbLTy-868vGZQj_fQ")
+                    .subscribe(res4 => console.log(res4));
             });
     }
 
--- src/app/services/knora.service.ts
+++ src/app/services/knora.service.ts
@@ -128,4 +128,146 @@ export class KnoraService {
         console.log(graveSearch);
         return this.knoraApiConnection.v2.search.doExtendedSearch(graveSearch);
     }
+
+    getPassageAuthor(authorIri: string) {
+        const bind = authorIri ? `BIND (<${authorIri}> AS ?author)` : "";
+        const graveSearch = `
+        PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+        PREFIX teimww: <http://0.0.0.0:3333/ontology/0826/teimww/simple/v2#>
+        CONSTRUCT {
+            ?passage knora-api:isMainResource true .
+            ?passage teimww:hasText ?text .
+        } WHERE {
+            ${bind}
+            ?passage a teimww:passage .
+            ?passage teimww:hasText ?text .
+            ?passage teimww:hasPage ?page .
+            ?passage teimww:occursIn ?book .
+            ?passage teimww:wasContributedBy ?contributor .
+            ?passage teimww:contains ?lexia .
+            ?passage teimww:isMentionedIn ?sPassage .
+            OPTIONAL {
+                ?passage teimww:hasTextHist ?textHist .
+            }
+            OPTIONAL {
+                ?passage teimww:hasPageHist ?pageHist .
+            }
+            OPTIONAL {
+                ?passage teimww:hasMarking ?marking .
+            }
+            OPTIONAL {
+                ?passage teimww:hasStatus ?status .
+            }
+            OPTIONAL {
+                ?passage teimww:hasFunctionVoice ?function .
+            }
+            OPTIONAL {
+                ?passage teimww:hasResearchField ?research .
+            }
+            OPTIONAL {
+                ?passage teimww:publicComment ?pubComment .
+            }
+
+            ?sPassage teimww:hasPage ?sPage .
+            ?sPassage teimww:occursIn ?sBook .
+            OPTIONAL {
+                ?sPassage teimww:hasText ?sText .
+            }
+
+            ?sBook teimww:bookTitle ?sBt .
+            ?sBook teimww:isWrittenBy ?sAuthor .
+
+            ?sAuthor teimww:firstName ?sFn .
+            ?sAuthor teimww:lastName ?sLn .
+
+            ?lexia teimww:lexiaTitle ?lt .
+
+            ?book teimww:bookTitle ?bt .
+            ?book teimww:isWrittenBy ?author .
+
+            ?author teimww:firstName ?fn .
+            ?author teimww:lastName ?ln .
+
+            ?contributor teimww:firstName ?conFn .
+            ?contributor teimww:lastName ?conLn .
+        }
+        OFFSET 0
+        `;
+        console.log(graveSearch);
+        return this.knoraApiConnection.v2.search.doExtendedSearch(graveSearch);
+    }
+
+    getAllBooks(): any {
+        const graveSearch = `
+        PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+        PREFIX teimww: <http://0.0.0.0:3333/ontology/0826/teimww/simple/v2#>
+        CONSTRUCT {
+            ?book knora-api:isMainResource true .
+            ?book teimww:bookTitle ?bookTitle .
+            ?book teimww:bookInternalId ?internalID .
+            ?book teimww:isWrittenBy ?author .
+            ?book teimww:edition ?edition .
+            ?book teimww:editionHist ?editionHist .
+            ?book teimww:createdDate ?createdDate .
+            ?book teimww:publishDate ?publishDate .
+            ?book teimww:firstPerformanceDate ?firstPerformanceDate .
+            ?book teimww:hasLanguage ?hasLanguage .
+            ?book teimww:hasGenre ?hasGenre .
+            ?book teimww:hasSubject ?hasSubject .
+            ?book teimww:performedBy ?performedBy .
+            ?book teimww:performedIn ?performedIn .
+            ?book teimww:lexiaAsBook ?lexiaAsBook .
+            } WHERE {
+                ?book a teimww:book .
+            OPTIONAL {
+                ?book teimww:bookTitle ?bookTitle .
+            }
+            OPTIONAL {
+                ?book teimww:bookInternalId ?internalID .
+            }
+            OPTIONAL {
+                ?book teimww:isWrittenBy ?author .
+            }
+            OPTIONAL {
+                ?book teimww:edition ?edition .
+            }
+            OPTIONAL {
+                ?book teimww:editionHist ?editionHist .
+            }
+            OPTIONAL {
+                ?book teimww:createdDate ?createdDate .
+            }
+            OPTIONAL {
+                ?book teimww:publishDate ?publishDate .
+            }
+            OPTIONAL {
+                ?book teimww:firstPerformanceDate ?firstPerformanceDate .
+            }
+            OPTIONAL {
+                ?book teimww:hasLanguage ?hasLanguage .
+            }
+            OPTIONAL {
+                ?book teimww:hasGenre ?hasGenre .
+            }
+            OPTIONAL {
+                ?book teimww:hasSubject ?hasSubject .
+            }
+            OPTIONAL {
+                ?book teimww:performedBy ?performedBy .
+            }
+            OPTIONAL {
+                ?book teimww:performedIn ?performedIn .
+            }
+            OPTIONAL {
+                ?book teimww:lexiaAsBook ?lexiaAsBook .
+            }
+        }
+        OFFSET 0
+        `;
+        console.log(graveSearch);
+        return this.knoraApiConnection.v2.search.doExtendedSearch(graveSearch);
+    }
+
+    getAllListNodeIris() {
+    }
 }
