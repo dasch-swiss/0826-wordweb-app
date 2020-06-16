756b07218073067b8229594268632abbd6e662cb
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2020-06-16 22:51:53 +0200

    preparing list in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {KnoraService} from "./services/knora.service";
+import {ListService} from "./services/list.service";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    readonly EMAIL = "root@example.com";
+    readonly PW = "test";
 
-    constructor() {
+    constructor(
+        private listService: ListService,
+        private knoraService: KnoraService) {
+    }
+
+    ngOnInit() {
+        this.knoraService.login(this.EMAIL, this.PW)
+            .subscribe(loginData => {
+                console.log(loginData);
+                this.prepareLists();
+            });
+    }
+
+    prepareLists() {
+        this.knoraService.getAllLists()
+            .subscribe(lists => {
+                lists.map(list => {
+                    this.knoraService.getList(list.id)
+                        .subscribe(data => {
+                            this.listService.setAllLists(data);
+                            // this.listService.print();
+                        });
+                });
+            });
     }
 }
