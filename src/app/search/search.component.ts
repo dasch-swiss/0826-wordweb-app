eec6b38748c15a243c23c62d004e68f79d3cfe44
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2020-06-16 22:52:35 +0200

    clean up

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
 
 @Component({
     selector: "app-search",
@@ -9,8 +8,7 @@ import {Router} from "@angular/router";
 export class SearchComponent implements OnInit {
     selectedSearch: string;
 
-    constructor(private router: Router) {
-    }
+    constructor() {}
 
     ngOnInit() {
         this.selectedSearch = "simple-search";
