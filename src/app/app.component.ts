9032248391c9c2a4494e0f6483271b224b1ac0ea
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2020-09-18 14:56:12 +0200

    Displaying app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {version} from "../../package.json";
 
 @Component({
     selector: "app-root",
@@ -6,6 +7,7 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
+    public version: string = version;
 
     constructor() {
     }
