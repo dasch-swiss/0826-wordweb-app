8c7405ddd2f8cc10eae3e4d83040df9a70e86be7
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2020-11-19 10:24:37 +0100

    format

--- src/app/services/backend-data.service.spec.ts
+++ src/app/services/backend-data.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from "@angular/core/testing";
 
-import { BackendDataService } from './backend-data.service';
+import {BackendDataService} from "./backend-data.service";
 
-describe('BackendDataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("BackendDataService", () => {
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: BackendDataService = TestBed.get(BackendDataService);
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        const service: BackendDataService = TestBed.get(BackendDataService);
+        expect(service).toBeTruthy();
+    });
 });
--- src/app/services/gravesearch-builder.service.spec.ts
+++ src/app/services/gravesearch-builder.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from "@angular/core/testing";
 
-import { GravsearchBuilderService } from './gravsearch-builder.service';
+import {GravsearchBuilderService} from "./gravsearch-builder.service";
 
-describe('GravesearchBuilderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe("GravesearchBuilderService", () => {
+    beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: GravsearchBuilderService = TestBed.get(GravsearchBuilderService);
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        const service: GravsearchBuilderService = TestBed.get(GravsearchBuilderService);
+        expect(service).toBeTruthy();
+    });
 });
