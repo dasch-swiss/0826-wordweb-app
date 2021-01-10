c3b101755b7924e2841f0250c1126e2544aa42b6
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2021-01-10 16:41:14 +0100

    editor search for passages (not finished)

--- src/app/categories/passage/passage.component.ts
+++ src/app/categories/passage/passage.component.ts
@@ -8,6 +8,10 @@ import {CreateUpdatePassageComponent} from "./create-update-passage/create-updat
 import {BookRefComponent} from "../../dialog/book-ref/book-ref.component";
 import {animate, state, style, transition, trigger} from "@angular/animations";
 import {FormControl, FormGroup} from "@angular/forms";
+import {TreeTableService} from "../../services/tree-table.service";
+import {KnoraService} from "../../services/knora.service";
+import {ListService} from "../../services/list.service";
+import {IDisplayedProperty, IMainClass} from "../../model/displayModel";
 
 @Component({
   selector: "app-passage",
@@ -22,17 +26,215 @@ import {FormControl, FormGroup} from "@angular/forms";
     ]
 })
 export class PassageComponent implements OnInit {
+    myPassage: IMainClass = {
+        name: "passage",
+        mainClass: {name: "passage", variable: "passage"},
+        props: [
+            {
+                name: "hasText",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasTextHist",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasPrefixDisplayedTitle",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasDisplayedTitle",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasPage",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasPageHist",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasResearchField",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasFunctionVoice",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasMarking",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasStatus",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasInternalComment",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasPassageComment",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "occursIn",
+                priority: 1,
+                res: {
+                    name: "book",
+                    props: [
+                        {
+                            name: "hasPrefixBookTitle",
+                            priority: 0,
+                            res: null
+                        },
+                        {
+                            name: "hasBookTitle",
+                            priority: 0,
+                            res: null
+                        }
+                    ]
+                }
+            },
+            {
+                name: "isMentionedIn",
+                priority: 1,
+                res: {
+                    name: "passage",
+                    props: [
+                        {
+                            name: "occursIn",
+                            valVar: "sBook",
+                            priority: 1,
+                            res: {
+                                name: "book",
+                                props: [
+                                    {
+                                        name: "hasBookTitle",
+                                        valVar: "sBookTitle",
+                                        priority: 1,
+                                        res: null
+                                    },
+                                    {
+                                        name: "hasEdition",
+                                        valVar: "sEdition",
+                                        priority: 1,
+                                        res: null
+                                    },
+                                    {
+                                        name: "isWrittenBy",
+                                        valVar: "sAuthor",
+                                        priority: 1,
+                                        res: {
+                                            name: "person",
+                                            props: [
+                                                {
+                                                    name: "hasFirstName",
+                                                    valVar: "sFirstName",
+                                                    priority: 1,
+                                                    res: null
+                                                },
+                                                {
+                                                    name: "hasLastName",
+                                                    valVar: "sLastName",
+                                                    priority: 1,
+                                                    res: null
+                                                }
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                name: "wasContributedBy",
+                priority: 1,
+                res: {
+                    name: "person",
+                    props: [
+                        {
+                            name: "hasFirstName",
+                            valVar: "cFirstName",
+                            priority: 0,
+                            res: null
+                        },
+                        {
+                            name: "hasLastName",
+                            valVar: "cLastName",
+                            priority: 0,
+                            res: null
+                        }
+                    ]
+                }
+            },
+            {
+                name: "contains",
+                priority: 1,
+                res: {
+                    name: "lexia",
+                    props: [
+                        {
+                            name: "hasLexiaTitle",
+                            priority: 0,
+                            res: null
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+
+    textRef: IDisplayedProperty = this.myPassage.props[0];
+    textHistRef: IDisplayedProperty = this.myPassage.props[1];
+    prefixDisptitleRef: IDisplayedProperty = this.myPassage.props[2];
+    disptitleRef: IDisplayedProperty = this.myPassage.props[3];
+    pageRef: IDisplayedProperty = this.myPassage.props[4];
+    pageHistRef: IDisplayedProperty = this.myPassage.props[5];
+    researchRef: IDisplayedProperty = this.myPassage.props[6];
+    functionRef: IDisplayedProperty = this.myPassage.props[7];
+    markingRef: IDisplayedProperty = this.myPassage.props[8];
+    statusRef: IDisplayedProperty = this.myPassage.props[9];
+    intCommentRef: IDisplayedProperty = this.myPassage.props[10];
+    passCommentRef: IDisplayedProperty = this.myPassage.props[11];
+    bookTitleRef: IDisplayedProperty = this.myPassage.props[12].res.props[1];
+    secBookTitleRef: IDisplayedProperty = this.myPassage.props[13].res.props[0].res.props[0];
+    priority = 0;
+    searchResults = [];
+
     columnsToDisplay: string[] = ["detail", "book", "text", "textHist", "page", "pageHist", "order", "references", "action"];
     dataSource: MatTableDataSource<Passage>;
     expandedElements: any[] = [];
     value: string;
     form: FormGroup;
+    researchFields: any[];
+    functionVoices: any[];
+    markings: any[];
+    status: any[];
 
     @ViewChild(MatSort, { static: true }) sort: MatSort;
 
     constructor(private apiService: ApiService,
                 private passageDialog: MatDialog,
-                private editionDialog: MatDialog) {
+                private editionDialog: MatDialog,
+                public listService: ListService,
+                private knoraService: KnoraService,
+                private treeTableService: TreeTableService) {
     }
 
     static customFilter(passage: Passage, filterValue: string): boolean {
@@ -45,19 +247,19 @@ export class PassageComponent implements OnInit {
     ngOnInit() {
         this.form = new FormGroup({
             text: new FormControl("", []),
-            textHistNull: new FormControl("", []),
+            textHistNull: new FormControl(false, []),
             textHist: new FormGroup({
                 txh: new FormControl("", [])
             }),
-            prefDisplayedTitleNull: new FormControl("", []),
+            prefDisplayedTitleNull: new FormControl(false, []),
             prefDisplayedTitle: new FormGroup({
                 prefdistit: new FormControl("", []),
             }),
-            pageNull: new FormControl("", []),
+            pageNull: new FormControl(false, []),
             page: new FormGroup({
                 pg: new FormControl("", [])
             }),
-            pagHistNull: new FormControl("", []),
+            pagHistNull: new FormControl(false, []),
             pageHist: new FormGroup({
                 pgh: new FormControl("", [])
             }),
@@ -65,52 +267,73 @@ export class PassageComponent implements OnInit {
             function: new FormControl("", []),
             marking: new FormControl("", []),
             status: new FormControl("", []),
-            intCommentNull: new FormControl("", []),
+            intCommentNull: new FormControl(false, []),
             intComment: new FormGroup({
                 intc: new FormControl("", [])
             }),
-            passCommentNull: new FormControl("", []),
+            passCommentNull: new FormControl(false, []),
             passComment: new FormGroup({
                 pc: new FormControl("", [])
             }),
             bookTitle: new FormControl("", []),
-            secBookTitleNull: new FormControl("", []),
+            secBookTitleNull: new FormControl(false, []),
             secBookTitle: new FormGroup({
                 secbt: new FormControl("", [])
             }),
             contributor: new FormControl("", []),
-            lexiaNull: new FormControl("", []),
+            lexiaNull: new FormControl(false, []),
             lexia: new FormGroup({
                 lex: new FormControl("", [])
             }),
-            extraNull: new FormControl("", []),
-            extra: new FormGroup({
-                ex: new FormControl("", [])
-            })
+            // extraNull: new FormControl( false, []),
+            // extra: new FormGroup({
+            //     ex: new FormControl("", [])
+            // })
         });
+
+        const researchNode = this.listService.getList("researchField").nodes;
+        this.researchFields = researchNode.reduce((acc, list) => this.treeTableService.flattenTree(acc, list), []);
+
+        const functionNode = this.listService.getList("functionVoice").nodes;
+        this.functionVoices = functionNode.reduce((acc, list) => this.treeTableService.flattenTree(acc, list), []);
+
+        const markingNode = this.listService.getList("marking").nodes;
+        this.markings = markingNode.reduce((acc, list) => this.treeTableService.flattenTree(acc, list), []);
+
+        const statusNode = this.listService.getList("status").nodes;
+        this.status = statusNode.reduce((acc, list) => this.treeTableService.flattenTree(acc, list), []);
+
         this.resetTable();
     }
 
     resetSearch() {
-        // this.form.get("internalId").reset("");
-        // this.form.get("creationDate").reset("");
-        // this.form.controls.firstNameNull.setValue(false);
-        // this.form.get("firstName").enable();
-        // this.form.get("firstName.fn").reset("");
-        // this.form.get("lastName").reset("");
-        // this.form.get("description").reset("");
-        // this.form.controls.birthNull.setValue(false);
-        // this.form.get("birth").enable();
-        // this.form.get("birth.bdate").reset("");
-        // this.form.controls.deathNull.setValue(false);
-        // this.form.get("death").enable();
-        // this.form.get("death.ddate").reset("");
-        // this.form.controls.activeNull.setValue(false);
-        // this.form.get("active").enable();
-        // this.form.get("active.adate").reset("");
-        // this.form.controls.extraNull.setValue(false);
-        // this.form.get("extra").enable();
-        // this.form.get("extra.ex").reset("");
+        this.form.get("text").reset("");
+        this.form.controls.textHistNull.setValue(false);
+        this.form.get("textHist").enable();
+        this.form.get("textHist.txh").reset("");
+        this.form.controls.prefDisplayedTitleNull.setValue(false);
+        this.form.get("prefDisplayedTitle").enable();
+        this.form.get("prefDisplayedTitle.prefdistit").reset("");
+        this.form.controls.pageNull.setValue(false);
+        this.form.get("page").enable();
+        this.form.get("page.pg").reset("");
+        this.form.controls.pagHistNull.setValue(false);
+        this.form.get("pageHist").enable();
+        this.form.get("pageHist.pgh").reset("");
+        this.form.get("research").reset("");
+        this.form.get("function").reset("");
+        this.form.get("marking").reset("");
+        this.form.get("status").reset("");
+        this.form.controls.intCommentNull.setValue(false);
+        this.form.get("intComment").enable();
+        this.form.get("intComment.intc").reset("");
+        this.form.controls.passCommentNull.setValue(false);
+        this.form.get("passComment").enable();
+        this.form.get("passComment.pc").reset("");
+        this.form.get("bookTitle").reset("");
+        this.form.controls.secBookTitleNull.setValue(false);
+        this.form.get("secBookTitle").enable();
+        this.form.get("secBookTitle.secbt").reset("");
     }
 
     onChange(event, groupName: string) {
@@ -214,6 +437,50 @@ export class PassageComponent implements OnInit {
 
     expansion(element) {
         this.contains(element, this.expandedElements) ? this.expandedElements = this.removeElement(element, this.expandedElements) : this.addElement(element, this.expandedElements);
+    }
+
+    search() {
+        console.log("Searching starts...");
+
+        // Sets internal ID property
+        if (this.form.get("text").value) {
+            this.textRef.searchVal1 = this.form.get("text").value;
+        } else {
+            this.textRef.searchVal1 = null;
+        }
+        // Sets first name property
+        if (this.form.controls.textHistNull.value) {
+            this.textHistRef.isNull = true;
+            this.textHistRef.searchVal1 = null;
+        } else {
+            this.textHistRef.isNull = false;
+            if (this.form.get("textHist.txh").value) {
+                this.textHistRef.searchVal1 = this.form.get("textHist.txh").value;
+            } else {
+                this.textHistRef.searchVal1 = null;
+            }
+        }
+        // Sets first name property
+        if (this.form.controls.prefDisplayedTitleNull.value) {
+            this.prefixDisptitleRef.isNull = true;
+            this.prefixDisptitleRef.searchVal1 = null;
+        } else {
+            this.prefixDisptitleRef.isNull = false;
+            if (this.form.get("prefDisplayedTitle.prefdistit").value) {
+                this.prefixDisptitleRef.searchVal1 = this.form.get("prefDisplayedTitle.prefdistit").value;
+            } else {
+                this.prefixDisptitleRef.searchVal1 = null;
+            }
+        }
+
+        this.knoraService.gravsearchQueryCount(this.myPassage, this.priority)
+            .subscribe(numb => console.log("amount", numb));
+
+        this.knoraService.gravseachQuery(this.myPassage, this.priority)
+            .subscribe(data => {
+                console.log("results", data);
+                this.searchResults = data;
+            });
     }
 
 }
