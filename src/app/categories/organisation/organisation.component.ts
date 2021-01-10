7e04790e03b0678842a77276f9049aa4b803b9fa
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2021-01-10 14:14:26 +0100

    editor search for companies

--- src/app/categories/organisation/organisation.component.ts
+++ src/app/categories/organisation/organisation.component.ts
@@ -6,6 +6,8 @@ import {MatTableDataSource} from "@angular/material/table";
 import {ApiService} from "../../services/api.service";
 import {CreateUpdateOrganisationComponent} from "./create-update-organisation/create-update-organisation.component";
 import {FormControl, FormGroup} from "@angular/forms";
+import {IDisplayedProperty, IMainClass} from "../../model/displayModel";
+import {KnoraService} from "../../services/knora.service";
 
 @Component({
     selector: "app-organisation",
@@ -13,6 +15,48 @@ import {FormControl, FormGroup} from "@angular/forms";
     styleUrls: ["../category.scss"]
 })
 export class OrganisationComponent implements OnInit {
+    myCompany: IMainClass = {
+        name: "company",
+        mainClass: {name: "company", variable: "company"},
+        props: [
+            {
+                name: "hasCompanyInternalId",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasCompanyTitle",
+                priority: 0,
+                res: null
+            },
+            {
+                name: "hasMember",
+                priority: 0,
+                res: {
+                    name: "person",
+                    props: [
+                        {
+                            name: "hasFirstName",
+                            priority: 0,
+                            res: null
+                        },
+                        {
+                            name: "hasLastName",
+                            priority: 0,
+                            res: null
+                        }
+                    ]
+                }
+            }
+        ]
+    };
+
+    internalIDRef: IDisplayedProperty = this.myCompany.props[0];
+    companyTitleRef: IDisplayedProperty = this.myCompany.props[1];
+    member: IDisplayedProperty = this.myCompany.props[2];
+    priority = 0
+    searchResults = [];
+
     displayedColumns: string[] = ["internalID", "name", "order", "references", "action"];
     dataSource: MatTableDataSource<Organisation>;
     value: string;
@@ -21,6 +65,7 @@ export class OrganisationComponent implements OnInit {
     @ViewChild(MatSort, {static: true}) sort: MatSort;
 
     constructor(private apiService: ApiService,
+                private knoraService: KnoraService,
                 private createOrganisationDialog: MatDialog) {
     }
 
@@ -28,38 +73,24 @@ export class OrganisationComponent implements OnInit {
         this.form = new FormGroup({
             internalId: new FormControl("", []),
             companyTitle: new FormControl("", []),
-            memberNull: new FormControl("", []),
+            memberNull: new FormControl(false, []),
             member: new FormGroup({
                 mem: new FormControl("", [])
             }),
-            extraNull: new FormControl("", []),
-            extra: new FormGroup({
-                ex: new FormControl("", [])
-            })
+            // extraNull: new FormControl("", []),
+            // extra: new FormGroup({
+            //     ex: new FormControl("", [])
+            // })
         });
         this.resetTable();
     }
 
     resetSearch() {
-        // this.form.get("internalId").reset("");
-        // this.form.get("creationDate").reset("");
-        // this.form.controls.firstNameNull.setValue(false);
-        // this.form.get("firstName").enable();
-        // this.form.get("firstName.fn").reset("");
-        // this.form.get("lastName").reset("");
-        // this.form.get("description").reset("");
-        // this.form.controls.birthNull.setValue(false);
-        // this.form.get("birth").enable();
-        // this.form.get("birth.bdate").reset("");
-        // this.form.controls.deathNull.setValue(false);
-        // this.form.get("death").enable();
-        // this.form.get("death.ddate").reset("");
-        // this.form.controls.activeNull.setValue(false);
-        // this.form.get("active").enable();
-        // this.form.get("active.adate").reset("");
-        // this.form.controls.extraNull.setValue(false);
-        // this.form.get("extra").enable();
-        // this.form.get("extra.ex").reset("");
+        this.form.get("internalId").reset("");
+        this.form.get("companyTitle").reset("");
+        this.form.controls.memberNull.setValue(false);
+        this.form.get("member").enable();
+        this.form.get("member.mem").reset("");
     }
 
     onChange(event, groupName: string) {
@@ -112,6 +143,31 @@ export class OrganisationComponent implements OnInit {
     }
 
     deleteRow(id: number) {
+    }
+
+    search() {
+        console.log("Searching starts...");
+        // Sets internal ID property
+        if (this.form.get("internalId").value) {
+            this.internalIDRef.searchVal1 = this.form.get("internalId").value;
+        } else {
+            this.internalIDRef.searchVal1 = null;
+        }
+        // Sets internal ID property
+        if (this.form.get("companyTitle").value) {
+            this.companyTitleRef.searchVal1 = this.form.get("companyTitle").value;
+        } else {
+            this.companyTitleRef.searchVal1 = null;
+        }
+
+        this.knoraService.gravsearchQueryCount(this.myCompany, this.priority)
+            .subscribe(numb => console.log("amount", numb));
+
+        this.knoraService.gravseachQuery(this.myCompany, this.priority)
+            .subscribe(data => {
+                console.log("results", data);
+                this.searchResults = data;
+            });
     }
 
 }
