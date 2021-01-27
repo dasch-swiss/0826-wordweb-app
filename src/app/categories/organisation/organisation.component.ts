dcbcb7be02295b64ac4cd84c9ebd5f1b5beb24a0
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2021-01-27 11:16:52 +0100

    editor: displaying companies in table

--- src/app/categories/organisation/organisation.component.ts
+++ src/app/categories/organisation/organisation.component.ts
@@ -8,14 +8,19 @@ import {CreateUpdateOrganisationComponent} from "./create-update-organisation/cr
 import {FormControl, FormGroup} from "@angular/forms";
 import {IDisplayedProperty, IMainClass} from "../../model/displayModel";
 import {KnoraService} from "../../services/knora.service";
-import {forkJoin} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 
 @Component({
     selector: "app-organisation",
     templateUrl: "./organisation.component.html",
-    styleUrls: ["../category.scss"]
+    styleUrls: ["../category2.scss"]
 })
 export class OrganisationComponent implements OnInit {
+    @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+    readonly PRIORITY = 0;
+    readonly MAX_RESOURCE_PER_RESULT = 25;
+
     myCompany: IMainClass = {
         name: "company",
         mainClass: {name: "company", variable: "company"},
@@ -55,16 +60,34 @@ export class OrganisationComponent implements OnInit {
     internalIDRef: IDisplayedProperty = this.myCompany.props[0];
     companyTitleRef: IDisplayedProperty = this.myCompany.props[1];
     memberRef: IDisplayedProperty = this.myCompany.props[2];
-    priority = 0;
-    searchResults = [];
 
-    displayedColumns: string[] = ["internalID", "name", "order", "references", "action"];
-    dataSource: MatTableDataSource<Organisation>;
+    searchResults = [];
+    amountResult: Observable<number>;
+    searchStarted = false;
+
+    displayedColumns: string[] = ["row", "hasCompanyInternalId", "hasCompanyTitle", "action"];
+    dataSource: MatTableDataSource<any>;
     value: string;
     form: FormGroup;
     members: any[];
 
-    @ViewChild(MatSort, {static: true}) sort: MatSort;
+    static customFilter(item: any, filterValue: string): boolean {
+        const containsInternalID = item.hasCompanyInternalId[0].value.indexOf(filterValue) > -1;
+        const containsTitle = item.hasCompanyTitle[0].value.toLowerCase().indexOf(filterValue) > -1;
+
+        return containsInternalID || containsTitle;
+    }
+
+    static customSorting(item: any, property: string) {
+        switch (property) {
+            case "hasCompanyInternalId":
+                return item.hasCompanyInternalId[0].value;
+            case "hasCompanyTitle":
+                return item.hasCompanyTitle[0].value;
+            default:
+                return item[property];
+        }
+    }
 
     constructor(private apiService: ApiService,
                 private knoraService: KnoraService,
@@ -86,14 +109,12 @@ export class OrganisationComponent implements OnInit {
         });
 
         this.prepareMembers();
-
-        this.resetTable();
     }
 
     prepareMembers() {
         this.knoraService.getMembersCount()
             .subscribe(amount => {
-                const maxOffset = Math.ceil(amount / 25);
+                const maxOffset = Math.ceil(amount / this.MAX_RESOURCE_PER_RESULT);
 
                 const requests = [];
 
@@ -127,38 +148,32 @@ export class OrganisationComponent implements OnInit {
         this.form.controls.memberNull.setValue(false);
         this.form.get("member").enable();
         this.form.get("member.mem").reset("");
+        // this.form.controls.extraNull.setValue(false);
+        // this.form.get("extra").enable();
+        // this.form.get("extra.ex").reset("");
     }
 
     onChange(event, groupName: string) {
         event.checked ? this.form.get(groupName).disable() : this.form.get(groupName).enable();
     }
 
-    resetTable() {
-        this.apiService.getOrganisations(true).subscribe((organisations) => {
-            console.log(organisations);
-            this.dataSource = new MatTableDataSource(organisations);
-            this.dataSource.sort = this.sort;
-        });
-    }
-
     applyFilter(filterValue: string) {
-        this.dataSource.filter = filterValue.trim().toLowerCase();
+        if (this.searchResults.length != 0) {
+            this.dataSource.filterPredicate = OrganisationComponent.customFilter;
+            this.dataSource.filter = filterValue.trim().toLowerCase();
+        }
     }
 
-    clear() {
+    clearFilter() {
         this.dataSource.filter = this.value = "";
     }
 
-    rowCount() {
-        return this.dataSource ? this.dataSource.filteredData.length : 0;
-    }
-
     create() {
-        this.createOrEditResource(false);
+        // this.createOrEditResource(false);
     }
 
-    editRow(organisation: Organisation) {
-        this.createOrEditResource(true, organisation);
+    edit(organisation: Organisation) {
+        // this.createOrEditResource(true, organisation);
     }
 
     createOrEditResource(editMod: boolean, resource: Organisation = null) {
@@ -172,17 +187,18 @@ export class OrganisationComponent implements OnInit {
         const dialogRef = this.createOrganisationDialog.open(CreateUpdateOrganisationComponent, dialogConfig);
         dialogRef.afterClosed().subscribe((data) => {
             if (data.refresh) {
-                this.resetTable();
+                // TODO Refresh the table
                 this.dataSource.sort = this.sort;
             }
         });
     }
 
-    deleteRow(id: number) {
+    export() {
     }
 
     search() {
-        console.log("Searching starts...");
+        this.searchStarted = true;
+
         // Sets internal ID property
         if (this.form.get("internalId").value) {
             this.internalIDRef.searchVal1 = this.form.get("internalId").value;
@@ -208,13 +224,32 @@ export class OrganisationComponent implements OnInit {
             }
         }
 
-        this.knoraService.gravsearchQueryCount(this.myCompany, this.priority)
-            .subscribe(numb => console.log("amount", numb));
+        this.amountResult = this.knoraService.gravsearchQueryCount(this.myCompany, this.PRIORITY);
 
-        this.knoraService.gravseachQuery(this.myCompany, this.priority)
+        this.knoraService.gravseachQuery(this.myCompany, this.PRIORITY)
             .subscribe(data => {
                 console.log("results", data);
                 this.searchResults = data;
+                this.dataSource = new MatTableDataSource(data);
+                this.dataSource.sort = this.sort;
+                this.dataSource.sortingDataAccessor = OrganisationComponent.customSorting;
+                this.searchStarted = false;
+            });
+    }
+
+    loadMoreResults() {
+        this.clearFilter();
+        this.searchStarted = true;
+
+        const offset = Math.floor(this.searchResults.length / this.MAX_RESOURCE_PER_RESULT);
+
+        this.knoraService.gravseachQuery(this.myCompany, this.PRIORITY, offset)
+            .subscribe(data => {
+                this.searchResults.push(...data);
+                this.dataSource = new MatTableDataSource(this.searchResults);
+                this.dataSource.sort = this.sort;
+                this.dataSource.sortingDataAccessor = OrganisationComponent.customSorting;
+                this.searchStarted = false;
             });
     }
 
