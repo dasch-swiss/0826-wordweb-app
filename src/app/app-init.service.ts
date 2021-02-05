51e7f2e60f814ae710c4629e7db31d4a9f2dfde0
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2021-02-05 15:58:21 +0100

    error message on init service, format

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -1,25 +1,25 @@
 import {Injectable} from "@angular/core";
-import {KnoraService} from "./services/knora.service";
 import {mergeMap} from "rxjs/operators";
 import {forkJoin, Observable} from "rxjs";
+import {KnoraService} from "./services/knora.service";
 import {ListService} from "./services/list.service";
 import {GravsearchBuilderService} from "./services/gravsearch-builder.service";
-import {List, ListNodeInfo} from "@dasch-swiss/dsp-js";
+import {ApiResponseError, List, ListNodeInfo} from "@dasch-swiss/dsp-js";
 
 export interface IAppConfig {
 
-  env: {
-    name: string;
-  };
-  ontologyIRI: string;
-  apiURL: string;
-  externalApiURL: string;
-  iiifURL: string;
-  appURL: string;
-  appName: string;
-  localData: string;
-  pagingLimit: number;
-  startComponent: string;
+    env: {
+        name: string;
+    };
+    ontologyIRI: string;
+    apiURL: string;
+    externalApiURL: string;
+    iiifURL: string;
+    appURL: string;
+    appName: string;
+    localData: string;
+    pagingLimit: number;
+    startComponent: string;
 }
 
 @Injectable({
@@ -27,37 +27,41 @@ export interface IAppConfig {
 })
 export class AppInitService {
 
-  static settings: IAppConfig;
+    static settings: IAppConfig;
 
-  constructor(
-      private knoraService: KnoraService,
-      private listService: ListService,
-      private gsBuilder: GravsearchBuilderService) {
-  }
+    constructor(
+        private _knoraService: KnoraService,
+        private _listService: ListService,
+        private _gsBuilder: GravsearchBuilderService) {
+    }
 
-  Init() {
+    Init() {
 
-    return new Promise<void>((resolve, reject) => {
-      // do your initialisation stuff here
-      // console.log("AppInitService.init() called");
-      AppInitService.settings = window["tempConfigStorage"] as IAppConfig;
+        return new Promise<void>((resolve, reject) => {
+            // do your initialisation stuff here
+            // console.log("AppInitService.init() called");
+            AppInitService.settings = window["tempConfigStorage"] as IAppConfig;
 
-      this.gsBuilder.apiURL = AppInitService.settings.apiURL;
+            this._gsBuilder.apiURL = AppInitService.settings.apiURL;
 
-      this.knoraService.knoraApiConnection = AppInitService.settings.apiURL;
+            this._knoraService.knoraApiConnection = AppInitService.settings.apiURL;
 
-      this.knoraService.login("root@example.com", "test")
-          .pipe(
-              mergeMap(() => this.knoraService.getAllLists("http://rdfh.ch/projects/0826")),
-              mergeMap((lists: ListNodeInfo[]) => forkJoin<Observable<List>>(lists.map((list: ListNodeInfo) => this.knoraService.getList(list.id))))
-          )
-          .subscribe((fullList: List[]) => {
-
-            fullList.forEach(list => this.listService.list = list);
-
-            // console.log("AppInitService: finished");
-            resolve();
-          }, (error) => console.error("Failed to connect", error));
-    });
-  }
+            this._knoraService.login("root@example.com", "tests")
+                .pipe(
+                    mergeMap(() => this._knoraService.getAllLists("http://rdfh.ch/projects/0826")),
+                    mergeMap((lists: ListNodeInfo[]) => forkJoin<Observable<List>>(lists.map((list: ListNodeInfo) => this._knoraService.getList(list.id))))
+                )
+                .subscribe((fullList: List[]) => {
+                    fullList.forEach(list => this._listService.list = list);
+                    resolve();
+                }, (error: ApiResponseError) => {
+                    const errorMsgElement = document.querySelector('.errorMsgElement');
+                    let message = "Application initialization failed";
+                    if (error) {
+                        message = error.error ? `${message}: ${error.error}` : `${message}: ${error}`;
+                    }
+                    errorMsgElement.textContent = message;
+                });
+        });
+    }
 }
