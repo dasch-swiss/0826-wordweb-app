2bed086f33238ea32db68ab2a4e6878866ca3992
Lukas Rosenthaler <lukas.rosenthaler@unibas.ch>
2021-11-15 22:22:55 +0100

    Era indicattor for all dates

--- src/app/services/knora.service.ts
+++ src/app/services/knora.service.ts
@@ -443,6 +443,7 @@ export class KnoraService {
                         return propValue.linkedResource ? this.processRes(propValue.linkedResource) : {};
                     }
                     case (propValue instanceof ReadDateValue): {
+                        console.log('propValue:', propValue);
                         return (propValue.date instanceof KnoraPeriod) ?
                             {
                                 id: propValue.id,
@@ -454,7 +455,8 @@ export class KnoraService {
                             {
                                 id: propValue.id,
                                 start: propValue.date.year,
-                                end: propValue.date.year
+                                sEra: propValue.date.era,
+                                end: propValue.date.year,
                             };
                     }
                 }
